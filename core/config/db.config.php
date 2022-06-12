6de230a6d5820fcba38fc7f13a71afa9ae1f2442
Niloy Saha <niloy.saha@cosmosgroup.com.bd>
2022-06-12 16:52:05 +0600

    First commit for web-analyzer

---
+++ core/config/db.config.php
@@ -0,0 +1,33 @@
+<?php
+defined('ROOT_DIR') or die(header('HTTP/1.0 403 Forbidden'));
+
+/*
+ * @author Balaji
+ * @name: Rainbow PHP Framework
+ * @copyright 2020 ProThemes.Biz
+ *
+ */
+
+// --- Database Settings ---
+
+// Database Hostname
+$dbHost = 'localhost';
+
+// Database Username
+$dbUser = 'root';
+
+// Database Password
+$dbPass = '123';
+
+// Database Name
+$dbName = 'webanalyzer';
+
+//Base URL (Without http:// & https://)
+$baseURL = 'localhost/web-analyzer/';
+
+//Item Purchase Code
+$item_purchase_code = '123';
+
+//Domain Security Code
+$authCode = '90b9930efc0230fdc6f36b16fa60e5fb';
+
